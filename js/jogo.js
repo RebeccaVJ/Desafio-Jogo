0b448bc25705b672a1b1e70dc35d429197f1aa16
RebeccaVJ <rebeccajorge@outlook.com>
2023-06-08 16:57:19 -0300

    projeto jogo

---
+++ js/jogo.js
@@ -0,0 +1,14 @@
+// const numeroAleatorio = Math.round(Math.random()*100);
+// const campo = $("#campo-input");
+// const botao = $("#botao-chute");
+// const caixaNumero = $("#caixa-number");
+
+// function verificaNumero() {
+//     var valCampo = campo.val();
+//     var caixaAcerto = $("#caixa-acerto");
+
+//     if (valCampo == numeroAleatorio) {
+
+//     }
+
+// }
